a1aa89634740668c27472829d0a4caecc24169b9
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-16 16:18:23 +0530

    data processing using streams

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-Implement the processData function in app.js:
-
-const fs = require('fs');
-const { Transform } = require('stream');
+const fs = require('fs')
+const { Transform } = require('stream')
 
 function processData(inputFilePath, outputFilePath) {
   // Implement this function
+  const readableStream = fs.createReadStream(inputFilePath, {
+    encoding: 'utf-8',
+  })
+
+  const writableStream = fs.createWriteStream(outputFilePath)
+
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      try {
+        const processedChunk = chunk.toUpperCase()
+        callback(null, processedChunk)
+      } catch (error) {
+        callback(error)
+      }
+    },
+  })
+
+  readableStream.on('error', (err) => console.log(err))
+  writableStream.on('error', (err) => console.log(err))
+  transformStream.on('error', (err) => console.log(err))
+
+  readableStream.pipe(transformStream).pipe(writableStream)
+
+  writableStream.on('finish', () => {
+    console.log('Data processing complete. Output written to', outputFilePath)
+  })
 }
 
-processData('input.txt', 'output.txt');
-
+processData('input.txt', 'output.txt')
